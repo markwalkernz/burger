48d30aaa81a8540138d0d8753dffddd22b2b3db8
markwalkernz <markwalkernz@gmail.com>
2017-12-01 14:11:28 -0600

    app rendering to basic html page

--- server.js
+++ server.js
@@ -14,6 +14,17 @@ app.use(bodyParser.urlencoded({ extended: true }));
 // static directory to be served
 app.use(express.static("./public"));
 
+// Set Handlebars.
+var exphbs = require("express-handlebars");
+
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+// Import routes and give the server access to them.
+var routes = require("./controllers/burgers_controller.js");
+
+app.use("/", routes);
+
 // Starts the server to begin listening
 // =============================================================
 app.listen(PORT, function() {
